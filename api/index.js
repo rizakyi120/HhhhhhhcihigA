b9c1f1f2b722885143809dd2d556fa33aac3afa1
⚡ top 1 <rizakyi91@gmail.com>
2025-02-22 15:57:51 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
     }
 
     // تحقق من صحة `code`
-    if (code !== 'FADAI7700ki') {
+    if (code !== 'FOXC4') {
         return res.status(403).json({ error: "Unauthorized access" });
     }
 
